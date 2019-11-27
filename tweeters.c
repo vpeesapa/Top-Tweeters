a20a7ce5baa2dbe9f145bfce091de6982f075650
Varun Peesapati <peesapativarun99@gmail.com>
2019-11-27 11:05:02 -0800

    Created gitignore

--- tweeters.c
+++ tweeters.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /*
  * Function that checks if the program has one additional command line argument
@@ -19,6 +21,7 @@ void checkArgs(int numArgs)
 int main(int numArgs,char** args)
 {
 	char* fileName = (char *)malloc(sizeof(char));
+	int fd;
 	
 	checkArgs(numArgs);
 
@@ -26,6 +29,17 @@ int main(int numArgs,char** args)
 	strcpy(fileName,args[1]);
 	// FIXME: Omit following line
 	printf("The name of the file is: %s\n",fileName);
+
+	// Opening the file
+	fd = open(fileName,O_RDONLY);
+
+	if(fd == -1) {
+		printf("Non-existent file.\n");
+		exit(0);
+	}
+
+	printf("The file has been opened.\n");
+	close(fd);
 
 	return 0;
 }
