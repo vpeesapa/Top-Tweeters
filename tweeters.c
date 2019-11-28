9cceccaadc51363b1bfab3115e54c28f823724e8
jhvoong <jhvoong@ucdavis.edu>
2019-11-27 22:17:57 -0800

    Matches java version

--- tweeters.c
+++ tweeters.c
@@ -21,19 +21,20 @@ typedef struct {
 } Tweet;
 
 // FILE DECLARATIONS
+Tweet** bubbleSort(Tweet** tweets, int numTweets);
 Tweet* findTweet(Tweet** tweets, char* name);
 int getNameColumnPosition(FILE* fp);
 bool isDelimiter(char c, char* delimiters);
 bool matches(char* str);
-Tweet* parseFile(FILE* fp);
+Tweet** parseFile(FILE* fp);
 void printTop10(FILE* fp);
 char** tokenize(char* line, char* delimiters);
 Tweet toTweet(char** data);
 
 Tweet* findTweet(Tweet** tweets, char* name) {
-    for (Tweet* tweet = tweets[0]; tweet != NULL; tweet++) {
-        if (!strcmp(tweet->name, name)) {
-            return tweet;
+    for (int i = 0; tweets[i] != NULL; i++) {
+        if (!strcmp(tweets[i]->name, name)) {
+            return tweets[i];
         }
     }
     return NULL;
@@ -51,10 +52,10 @@ int getNameColumnPosition(FILE* fp) {
     tokenizedLine = tokenize(line, "\n,");
     
     nameFound = false;
-    for (int col = 0; col < sizeof(tokenizedLine) / sizeof(char); col++) {
+    for (int col = 0; tokenizedLine[col] != NULL; col++) {
         // check for duplicate name columns
         if (matches(tokenizedLine[col]) && nameFound) {
-            RAISE_ERROR("Invalid Input Format\n");
+            RAISE_ERROR("1Invalid Input Format\n");
         }
 
         if (matches(tokenizedLine[col])) {
@@ -64,7 +65,7 @@ int getNameColumnPosition(FILE* fp) {
     }
 
     if (!nameFound) {
-        RAISE_ERROR("Invalid Input Format\n");
+        RAISE_ERROR("2Invalid Input Format\n");
     }
 
     return namePos;
@@ -86,7 +87,7 @@ bool matches(char* str) {
 }
 
 
-Tweet* parseFile(FILE* fp) {
+Tweet** parseFile(FILE* fp) {
     char** fileData;
     char line[MAX_LINE_LENGTH];
     bool nameFound;
@@ -117,27 +118,59 @@ Tweet* parseFile(FILE* fp) {
             tweet->count = 1;
             tweets[numTweets++] = tweet;
         }
+        if (i % 1000 == 0) {
+        printf("%d\n", i);}
     }
 
-    return NULL;
+    return bubbleSort(tweets, numTweets);
 }
 
 
 
 // TODO NOTE: don't use strlen use memcpy, etc
 void printTop10(FILE* fp) {
-    Tweet** tweet = parseFile(fp);
+    Tweet** tweets = parseFile(fp);
+
+    for (int i = 0; tweets[i] != NULL && i < 10; i++) {
+        printf("%s: %d\n", tweets[i]->name, tweets[i]->count);
+    }
     // TODO get position of tweeter column from header on line 1
     // TODO get rest of lines of fp, strtok for ',', check if tweeterCol
     // exists, then count into a hashtable
-    // TODO sort and print
+    // TODO bubbleSort and print
 }
 
-char* trimSpaces(char* str) {
+
+Tweet** bubbleSort(Tweet** tweets, int numTweets) {
+    Tweet* tweetL;
+    Tweet* tweetR;
+    Tweet temp;
+
+    if (numTweets < 1) {
+        return tweets;
+    }
+
+    for (int i = 0; i < numTweets - 1; i++) {
+        for (int j = 0; j < numTweets - i - 1; j++) {
+            tweetL = tweets[j];
+            tweetR = tweets[j + 1];
+
+            if (tweetR->count > tweetL->count) {
+                temp = *tweetL;
+                *tweetL = *tweetR;
+                *tweetR = temp;
+            }
+        }
+    }
+    return tweets;
+}
+
+
+char* trim(char* str) {
     char* ptr;
 
     // remove leading spaces
-    while (*str == ' ') {
+    while (*str == ' ' || *str == '\r') {
         str++;
     }
 
@@ -147,7 +180,7 @@ char* trimSpaces(char* str) {
         ptr++;
     }
     ptr--;
-    while (*ptr == ' ' && ptr != str) { // remove trailing spaces
+    while ((*ptr == ' ' || *ptr == '\r') && ptr != str) { // remove trailing spaces
         ptr--;
     }
 
@@ -165,38 +198,29 @@ char** tokenize(char* line, char* delimiters) {
     int size; // represents current size of element
 
     // dynamic allocation
-    elements = (char**)malloc(MAX_LINE_LENGTH * sizeof(char));
-    for (int i = 0; i < MAX_LINE_LENGTH; i++) {
+    elements = (char**)malloc((MAX_LINE_LENGTH + 1) * sizeof(char*));
+    for (int i = 0; i < MAX_LINE_LENGTH + 1; i++) {
         elements[i] = (char*)malloc(MAX_LINE_LENGTH * sizeof(char));
     }
-    element = (char*)malloc(MAX_LINE_LENGTH * sizeof(char) + 1); // + 1 for null terminator
+    element = (char*)malloc((MAX_LINE_LENGTH + 1) * sizeof(char)); // + 1 for null terminator
 
     // tokenize line by delimiters
     size = numElements = 0;
-    for (int i = 0; line[i] != '\0'; i++) { // loop until end of line
+        for (int i = 0; line[i] != '\0'; i++) { // loop until end of line
         // check if char is a delimiter and save 1 byte for null terminator
         if (!isDelimiter(line[i], delimiters) && size < MAX_LINE_LENGTH - 1) {
             element[size++] = line[i];
         } else {
             element[size] = '\0'; // TODO is this needed?
-            element = trimSpaces(element);
+            element = trim(element);
             memcpy(elements[numElements++], element, size + 1);
             size = 0;
         }
     }
 
+    elements[numElements] =  '\0';
+
     return elements;
-}
-
-
-Tweet toTweet(char** data) {
-    Tweet tweet;
-    return tweet;
-}
-
-
-void updateTweets(char** tokenizedLine, Tweet* tweet) {
-
 }
 
 
@@ -205,13 +229,13 @@ int main(int argc, char **argv) {
 
     // check command line arguments
     if (argc != 2) {
-        RAISE_ERROR("Invalid Input Format\n");
+        RAISE_ERROR("3Invalid Input Format\n");
     }
    
     // check if can open fp
     if ((fp = fopen(argv[1], "r")) == NULL) {
         fclose(fp);
-        RAISE_ERROR("Invalid Input Format\n");
+        RAISE_ERROR("4Invalid Input Format\n");
     }
 
     printTop10(fp);
